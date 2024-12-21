1e0113dc35ffdb80e55f7f89157d24af09685c59
Selsa Trikartika <selsakartika03@gmail.com>
2024-12-21 17:43:40 +0700

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,19 @@
+
+public class Account {
+
+    public void changePin(String oldPin, String newPin1, String newPin2) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'changePin'");
+    }
+
+    public String getBalance() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getBalance'");
+    }
+
+    public Object getPin() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPin'");
+    }
+
+}
---
+++ Transaction.java
@@ -0,0 +1,9 @@
+
+public class Transaction {
+
+    public void execute() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'execute'");
+    }
+
+}
